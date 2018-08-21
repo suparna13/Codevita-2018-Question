eef4c32bfe2ff09666d61a2ac8190a8c2bb71e1b
Suparna Nandkumar Raut <rautsuparna13.sr@gmail.com>
2018-08-21 23:55:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import math
+
+v1=int(input())
+r1=int(input())
+v2=int(input())
+r2=int(input())
+time=int(input())
+angle_A=90+(time*v1)
+angle_B=90+(time*v2)
+if angle_A > angle_B:
+    final_angle=angle_A-angle_B
+else:
+    final_angle=angle_B-angle_A
+    
+rad=math.radians(final_angle)
+x=math.cos(rad)
+Third_side=(r1*r1)+(r2*r2)-(2*r1*r2*x)
+side=Third_side ** 0.5
+print("%.2f" % round(side,2))
+
